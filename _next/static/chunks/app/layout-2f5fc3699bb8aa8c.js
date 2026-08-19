009dbdc83769178bf930bd6754443d5abe3f3c98
Louis-7 <Louis-7@users.noreply.github.com>
2026-08-19 08:59:15 +0000

    deploy: 13831d93e45e31168064bfb82e117c90c473fe90

---
+++ _next/static/chunks/app/layout-2f5fc3699bb8aa8c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{28963:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}},38586:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,28963,23)),Promise.resolve().then(l.t.bind(l,68380,23))},68380:()=>{}},e=>{e.O(0,[557,743,441,255,358],()=>e(e.s=38586)),_N_E=e.O()}]);
